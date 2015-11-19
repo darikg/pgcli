16657ef0caad82bc2ee9ea81821d56c0a5287e5f
Darik Gamble <darik.gamble.spam@gmail.com>
2015-11-19 18:57:26 -0500

    split_by_binary_operators method

--- pgcli/packages/pgliterals/main.py
+++ pgcli/packages/pgliterals/main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 
 root = os.path.dirname(__file__)
 literal_file = os.path.join(root, 'pgliterals.json')
@@ -13,3 +14,21 @@ def get_literals(literal_type):
         returns a tuple of literal values of that type"""
 
     return tuple(literals[literal_type])
+
+
+binary_operators = set(get_literals('binary_operators'))
+binary_operator_regex = '|'.join(re.escape(op) for op in binary_operators)
+binary_operator_regex = re.compile(binary_operator_regex)
+
+
+def split_by_binary_operators(word):
+    """
+    >>> split_by_binary_operators('')
+    ['']
+    >>> split_by_binary_operators('foo')
+    ['foo']
+    >>> split_by_binary_operators('foo-bar')
+    ['foo', 'bar']
+    """
+    return binary_operator_regex.split(word)
+
