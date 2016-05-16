d49afac866855084846a77e99acf3597834301da
Darik Gamble <darik.gamble.spam@gmail.com>
2016-05-16 19:11:35 -0400

    What the

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requirements = [
             'click >= 4.1',
             'Pygments >= 2.0',  # Pygments has to be Capitalcased. WTF?
             'prompt_toolkit>=1.0.0,<1.1.0',
-            'psycopg2 >= 2.6.0',
+            'psycopg2 >= 2.5.4',
             'sqlparse == 0.1.19',
             'configobj >= 5.0.6',
             'humanize >= 0.5.1',
