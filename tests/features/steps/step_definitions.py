361fd0555618205102c95457186c4e6558a7465f
Darik Gamble <darik.gamble.spam@gmail.com>
2016-07-21 18:42:45 -0400

    huh

--- tests/features/steps/step_definitions.py
+++ tests/features/steps/step_definitions.py
@@ -296,7 +296,7 @@ def _expect_exact(context, expected, timeout):
         context.cli.expect_exact(expected, timeout=timeout)
     except:
         # Strip color codes out of the output.
-        actual = re.sub(r'\x1b\[([0-9A-Za-z;?])+[m|K]?', '', context.cli.before)
+        actual = re.sub(r'\x1b\[([0-9A-Za-z;?])+[m|K]?', '', str(context.cli.before))
         raise Exception('Expected:\n---\n{0}\n---\n\nActual:\n---\n{1}\n---'.format(
             expected,
             actual))
